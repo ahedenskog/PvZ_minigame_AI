f19976f54d0882706c82d453c80bf811027b18cf
ahedenskog <albin.hedenskog@elev.ga.ntig.se>
2022-12-09 11:09:27 +0100

    Stuff does stuff but not done (not even close)

--- sandbox/test-script.py
+++ sandbox/test-script.py
@@ -1,13 +1,12 @@
 import pyautogui
 
 screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
-print(screenWidth, screenHeight)
-
 currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
-print(currentMouseX, currentMouseY)
 
-pyautogui.moveTo(100, 150) # Move the mouse to XY coordinates.
-print(currentMouseX, currentMouseY)
-
-hello = pyautogui.locateOnScreen('C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/sandbox/chrome.png', confidence=5)  # returns (left, top, width, height) of first place it is found
-print(hello)
+oi = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/sandbox/chrom.PNG", confidence=0.6)
+pyautogui.click(oi)
+pyautogui.keyDown('ctrl')
+pyautogui.press('t')
+pyautogui.keyUp('ctrl')
+pyautogui.typewrite('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+pyautogui.press('enter')
--- whack-a-zombie.py
+++ whack-a-zombie.py
@@ -1,1 +1,16 @@
 import pyautogui
+
+screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
+currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
+
+minigame = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/minigameBtn.png", confidence=0.9)
+pyautogui.click(minigame)
+
+Whacky = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/WAZ.PNG", confidence=0.9)
+pyautogui.click(Whacky)
+
+NewGame = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/newGame1.PNG", confidence=0.9)
+pyautogui.click(NewGame)
+
+Confirm = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/newGame2.PNG", confidence=0.9)
+pyautogui.click(Confirm)

1419178abcb1a43918ed2d3490c7277ed4fa9df8
ahedenskog <albin.hedenskog@elev.ga.ntig.se>
2023-05-02 11:49:16 +0200

    Final commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an abandonned attempt at using image recognition to cheat at Whack-a-Zombie. It was a fun project, but I never got it to work well enough to be useful. I'm leaving it here for posterity.
--- whack-a-zombie.py
+++ whack-a-zombie.py
@@ -3,31 +3,30 @@ import pyautogui
 screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
 currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
 
-# minigame = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/minigameBtn.png", confidence=0.9)
-# if minigame != None:
-#     pyautogui.click(minigame)
+minigame = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/minigameBtn.png", confidence=0.9)
+if minigame != None:
+    pyautogui.click(minigame)
 
-# Whacky = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/WAZ.PNG", confidence=0.9)
-# if Whacky != None:
-#     pyautogui.click(Whacky)
+Whacky = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/WAZ.PNG", confidence=0.9)
+if Whacky != None:
+    pyautogui.click(Whacky)
 
-# NewGame = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/newGame1.PNG", confidence=0.9)
-# if NewGame != None:
-#     pyautogui.click(NewGame)
+NewGame = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/newGame1.PNG", confidence=0.9)
+if NewGame != None:
+    pyautogui.click(NewGame)
 
-# Confirm = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/newGame2.PNG", confidence=0.9)
-# if Confirm != None:
-#     pyautogui.click(Confirm)
+Confirm = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/whack-a-zombie/newGame2.PNG", confidence=0.9)
+if Confirm != None:
+    pyautogui.click(Confirm)
 
 while True:
     Zombie = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/zombie.png", confidence=0.6)
-    ZombieToo = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/zombieToo.png", confidence=0.6)
     Buckethead = pyautogui.locateOnScreen("C:/Users/albin.hedenskog/Documents/GitHub/PvZ_minigame_AI/assets/buckethead.png", confidence=0.6)
-    if ZombieToo != None:
-        print("I see a zombie at:", Zombie, ZombieToo)
+    if Zombie != None:
+        print("I see a zombie at:", Zombie)
+        pyautogui.click(Zombie)
     else:
-        print("I'm looking for zombies...")
-        #pyautogui.click(Zombie)
+        print("I'm looking for zombies...")  
     # if Buckethead != None:
-    #     print("I see a buckethead at:", Buckethead)    
-    #     pyautogui.click(Buckethead)
+    #      print("I see a buckethead at:", Buckethead)    
+    #      pyautogui.click(Buckethead)
